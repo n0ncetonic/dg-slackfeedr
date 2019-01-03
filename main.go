772db6ebd8a3d9a0ef9d66c59ef5ea685da8f258
n0ncetonic <n0ncetonic@users.noreply.github.com>
2019-01-03 12:47:37 -0800

    It works!

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/BlacksunLabs/drgero/event"
 	"github.com/BlacksunLabs/drgero/mq"
 )
 
+// Message is just a placeholder for the slack post body
+type Message struct {
+	Text string `json:"text"`
+}
+
+var whURL string
+
 var m = new(mq.Client)
 
+func post(message string) error {
+	payload := Message{Text: message}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("unable to marshal payload: %v", err)
+		return err
+	}
+
+	_, err = http.Post(whURL, "application/json", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		log.Printf("failed to post to slack")
+		return err
+	}
+
+	return nil
+}
+
 func main() {
+	whURL = os.Getenv("SLACK_WEBHOOK")
 	err := m.Connect("amqp://guest:guest@localhost:5672")
 	if err != nil {
 		fmt.Printf("unable to connect to RabbitMQ : %v", err)
@@ -58,7 +88,19 @@ func main() {
 				<-forever
 			}
 			// Feed Parsing/posting here
+			if event.UserAgent == "feedmonitor" {
+				log.Printf("%v", event.Message)
+			}
 
+			feedMap := make(map[string]interface{})
+
+			err = json.Unmarshal([]byte(event.Message), &feedMap)
+			if err != nil {
+				log.Printf("failed to unmarshal feed event: %v", err)
+			}
+
+			data := fmt.Sprintf("%s %s", feedMap["title"], feedMap["link"])
+			post(data)
 		}
 	}()
 
